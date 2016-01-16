7f9fb193ef57d2c2e387eac88925a60f5d56893e
Harry James <harryjamesuk@users.noreply.github.com>
2016-01-16 15:45:39 +0000

    Set up SharedPreferences
    Also removed the "Hello world" TextView and put a Blank EditText in the layout instead.
    VCS Set up with Git.

--- app/src/main/java/com/harryjamesuk/sharedpreferencesapp/MainActivity.java
+++ app/src/main/java/com/harryjamesuk/sharedpreferencesapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.harryjamesuk.sharedpreferencesapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String PREFS_FILE = "com.harryjamesuk" + ".sharedpreferencesapp" + ".preferences";
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
+        mEditText = (EditText) findViewById(R.id.editText);
     }
 }
